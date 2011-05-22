012995566b1a429d340b062f1b402cf39e78dec8
Cody Robbins <cody@sakuzaku.com>
2011-05-21 23:22:25 -0400

    Added some documentation.

--- lib/cody_robbins/classifyize.rb
+++ lib/cody_robbins/classifyize.rb
@@ -1,7 +1,16 @@
 module CodyRobbins
-  module Classifyize
-    def classifyize
-      to_s.classify.constantize
+  module ToClass
+    # Converts the object in question to Camel case and then tries to find a declared constant with the corresponding name. This combines the functionality of ActiveSupport’s [`camelize`](http://rubydoc.info/gems/activesupport/ActiveSupport/Inflector:camelize) and [`constantize`](http://rubydoc.info/gems/activesupport/ActiveSupport/Inflector:constantize) methods, and allows you to easily get a class from its name with one simple method invokation.
+    #
+    # @return [Class] The class corresponding to the representation of its name.
+    #
+    # @example
+    #     :user.to_class       #=> User
+    #     'user'.to_class      #=> User
+    #     :blog_post.to_class  #=> BlogPost
+    #     'blog_post'.to_class #=> BlogPost
+    def to_class
+      to_s.camelize.constantize
     end
   end
 end
